1f2c59be5ebadd862293c681a238719df80c9bef
Shubham Sharma <s_sharma2@me.iitr.ac.in>
2023-08-30 14:08:28 +0530

    upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, jsonify
+
+import openai
+
+openai.api_key = "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"
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+def index2():
+    return render_template('style.css')
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    chat_messages = [{'role': 'system', 'content': 'You are a helpful assistant.'}, {'role': 'user', 'content': input}]
+    return get_openai_response(chat_messages)
+
+def get_openai_response(messages):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=100,
+    )
+
+    return response['choices'][0]['message']['content']
+
+
+if __name__ == '__main__':
+    app.run()
